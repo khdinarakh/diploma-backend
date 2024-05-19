018554a17e67a134eb93a72b78dc502ab200e79b
Adil Zhexenov <adilzhexenoff@gmail.com>
2024-05-19 23:29:43 +0500

    update index.js

--- services/bcrypt.js
+++ services/bcrypt.js
@@ -1,9 +1,9 @@
-import { genSalt, hash, compare } from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export const hashPassword = async (passwd, saltRounds = 10) => {
   try {
-    const salt = await genSalt(saltRounds);
-    return await hash(passwd, salt);
+    const salt = await bcrypt.genSalt(saltRounds);
+    return await bcrypt.hash(passwd, salt);
   } catch (error) {
     console.error(error);
   }
@@ -11,7 +11,7 @@ export const hashPassword = async (passwd, saltRounds = 10) => {
 
 export const isValidPassword = async (passwordFromRequest, hashedPassword) => {
   try {
-    return await compare(passwordFromRequest, hashedPassword);
+    return await bcrypt.compare(passwordFromRequest, hashedPassword);
   } catch (error) {
     console.error(error);
   }
